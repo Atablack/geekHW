a23ff435e524eba6370fcd66048209eee5385ca9
Atablack <79922783+Atablack@users.noreply.github.com>
2023-01-05 15:07:23 +0100

    Add files via upload

---
+++ js2_hw1/main.js
@@ -0,0 +1,24 @@
+// var lang = prompt()
+
+// if (lang == 'ru') {
+//     console.log(['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']);
+// }   else if (lang == 'en') {
+//     console.log(['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']);
+// }   else {
+//     alert('ERROR')
+// }
+
+var lang = prompt()
+
+switch (lang) {
+    case 'ru':
+        console.log(['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']);
+        break;
+    case 'en':
+        console.log(['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']);
+        break;
+    default:
+        alert('ERROR')
+        break;
+}
+
