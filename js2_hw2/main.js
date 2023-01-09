bf7b75c926f432df03e9dc6c74821e8fce028677
Atablack <79922783+Atablack@users.noreply.github.com>
2023-01-09 17:28:03 +0100

    сделал дз 2

---
+++ js2_hw2/main.js
@@ -0,0 +1,38 @@
+//1.
+// let list = [10, 20, 30, 50, 235, 3000, 534, 222, 298, 7667, 16, 434, 12, 1111]
+
+// for (element of list) {
+//     if (String(element)[0] === '1') {
+//         console.log(element);
+//     } else if (String(element)[0] === '2') {
+//         console.log(element);
+//     } else if (String(element)[0] === '5') {
+//         console.log(element);
+//     }
+// }
+
+
+// 2.
+//  for (let i = 20; i >= 0; i--) {
+//     console.log(i);
+// }
+
+
+//3.
+// let trafficLight = prompt('введите цвет светофора')
+
+// let colors = {
+//     red: 'красный',
+//     green: 'зеленый',
+//     yellow: 'желтый'
+// }
+
+// if (trafficLight === colors.red) {
+//     alert ('стоп')
+// } else if (trafficLight === colors.green) {
+//     alert ('можно идти')
+// } else if (trafficLight === colors.yellow) {
+//     alert ('подождите')
+// } else {
+//     alert ('введите цвета: "красный", "зеленый", "желтый"')
+// }
