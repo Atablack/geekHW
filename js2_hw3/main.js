2fb3e8108e59d2ca3480e1d7c038c0479e655ac5
Atablack <79922783+Atablack@users.noreply.github.com>
2023-01-13 09:04:18 +0100

    dz 3

---
+++ js2_hw3/main.js
@@ -0,0 +1,50 @@
+// 1.
+
+// let first = Number(prompt('write first number'))
+// let second = Number(prompt('wrrite second number'))
+
+// let num = (a, b) => {
+//     if (a < b) {
+//         console.log(a);
+//     } else if (a == b) {
+//         console.log('equal numbers');
+//     } else {
+//         console.log(b);
+//     }
+// }
+
+// num(first, second)
+
+
+// 2.
+
+// let word = () => {
+//     let wordLength = prompt('write any word')
+//     console.log(wordLength.length);
+// }
+
+// word()
+
+
+// 3.
+// let numA = Number(prompt('write first number'))
+// let numB = prompt('write sign (+ - / *)')
+// let numC = Number(prompt('write second number'))
+
+// const calc = (a, b, c) => {
+//     if (b === '+') {
+//         console.log(a + c);
+//     } else if (b === '-') {
+//         console.log(a - c);
+//     } else if (b === '*') {
+//         console.log(a * c);
+//     } else if (a / 0 && c === 0) {
+//         alert('you cant divide by zero');
+//     } else if (b === '/') {
+//         console.log(a / c);
+//     } else {
+//         alert('write sign like (+ - / *)');
+//     }
+// } 
+ 
+// calc (numA, numB, numC)
