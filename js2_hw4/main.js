f887199db2318220d26cf623fbe27e1826581a42
Atablack <79922783+Atablack@users.noreply.github.com>
2023-01-17 22:13:11 +0600

    Add files via upload

---
+++ js2_hw4/main.js
@@ -0,0 +1,44 @@
+// 1
+// let count = document.getElementById('count')
+// let minus = document.getElementById('minus')
+// let plus = document .getElementById('plus')
+// let clear = document.getElementById('clear')
+// let num = 0
+
+// minus.addEventListener('click', e => {
+//     count.style.color = 'red';
+//     if (num == 0) {
+//         num
+//     } else  {
+//         num--
+//         count.innerHTML = num
+//     }
+// })
+
+// plus.addEventListener('click', e => {
+//     count.style.color = 'green';
+//     num++;
+//     count.innerHTML = num
+// })
+
+// clear.addEventListener('click', e => {
+//     num = 0
+//     count.innerHTML = num
+// })
+
+
+// 2 
+
+// let block = document.getElementById('block')
+// block.style.backgroundColor = 'pink'
+// block.style.width = '500px'
+// block.style.height = '500px'
+// block.style.border = 'solid 5px'
+
+// const posX = document.getElementById('posX')
+// const posY = document.getElementById('posY')
+
+// block.addEventListener('mousemove', event => {
+//     posX.innerHTML = event.screenX
+//     posY.innerHTML = event.screenY
+// })
